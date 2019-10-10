4c196f673e9fd0a7b95890de04025d470b2b05f3
Abborren <elias.renman00@gmail.com>
2019-10-10 21:44:43 +0200

    added comments

--- lib/Rules/Required.ts
+++ lib/Rules/Required.ts
@@ -1,6 +1,13 @@
 import {Rule} from "./Rule";
 
+/**
+ * Required rule class checks that a variable is in a payload.
+ */
 export class Required extends Rule {
+
+    /**
+     * @inheritDoc
+     */
     constructor() {
         super('required');
     }
