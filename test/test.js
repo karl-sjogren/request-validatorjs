043befedd4503277ac8310e9cdfdeff60c94f5f5
Abborren <elias.renman00@gmail.com>
2019-10-10 20:29:41 +0200

    changed in test

--- test/test.js
+++ test/test.js
@@ -4,14 +4,10 @@ var index = require('../dist/index.js').RequestValidator;
 var requestValidator = index.RequestValidator;
 var rule = index.Rule;
 
-describe('getPlural function test', () => {
+describe('RequestValidaor testing', () => {
     it('should Return at least one rule', () => {
         var result = new requestValidator();
         expect(result.rules.length > 0);
-    });
-    it('should return Girls', () => {
-        var result = index.getPlural('Girl');
-        expect(result).to.equal('Girls');
     });
 
 });
